3b6d421547a0d1f0c6f03d2d153b9c58ca71a8e0
spiderocta <cryptofer@tuta.io>
2022-06-29 03:01:20 +0200

    hardhat config with rinkeby

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,7 +3,7 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
+    rinkeby: {
       url: 'https://eth-rinkeby.alchemyapi.io/v2/6i5L_tok3g5wamJSJN1GR-jpCEuZrBg9',
       accounts: ['20f4e40d187e608285aa8f5f4223004d2b474b27002498778c144b0a82db3c72'],
     },
