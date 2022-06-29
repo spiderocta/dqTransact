70e07f97304a5b654492e1fe990a5f5856e8181f
spiderocta <cryptofer@tuta.io>
2022-06-29 03:00:47 +0200

    hardhat config with rinkeby

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/6i5L_tok3g5wamJSJN1GR-jpCEuZrBg9',
+      accounts: ['20f4e40d187e608285aa8f5f4223004d2b474b27002498778c144b0a82db3c72'],
+    },
+  },
 };
