532feda89ad4f7eaffb6e480eb00b3cbb26b035e
spiderocta <cryptofer@tuta.io>
2022-06-29 04:07:32 +0200

    finished transaction functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () =>{
                 <NavBarItem key={item + index} title={item} />
                 ))}
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                    Login
+                    Login using mobile wallet
                 </li>
             </ul>
             {/* for mobile navigations */}
