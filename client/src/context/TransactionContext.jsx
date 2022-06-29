40929f1c8f61813e1e56adc9cb8ce4557d492750
spiderocta <cryptofer@tuta.io>
2022-06-29 03:38:50 +0200

    connect wallet functionality

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import { contractABI, contractAddress } from "../utils/constants";
+
+export const TransactionContext = React.createContext();
+
+const { ethereum } = window;
+
+const getEthereumContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
+    console.log({provider , signer,transactionsContract })
+//   return transactionsContract;
+};
+
+export const TransactionsProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const checkIfWalletIsConnect = async () => {
+    try {
+      if (!ethereum) return alert("Please install MetaMask.");
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+
+        getAllTransactions();
+      } else {
+        console.log("No accounts found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install MetaMask.");
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts", });
+
+      setCurrentAccount(accounts[0]);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
+
+
+    return (
+        <TransactionContext.Provider value={{connectWallet}}>
+            { children }
+        </TransactionContext.Provider>
+    )
+}
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {TransactionsProvider} from './context/TransactionContext'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <TransactionsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionsProvider>
 )
