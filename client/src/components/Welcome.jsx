2858ab7da1d55dbbaf008b0f0524d61cc9c2b795
spiderocta <cryptofer@tuta.io>
2022-07-04 23:25:01 +0200

    index slight change

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -41,7 +41,7 @@ const Welcome = () => {
             Send DEQEX <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on DEQEX.
+            Explore the crypto world. Buy Assets and Send cryptocurrencies to anyone easily on DEQEX.
           </p>
           {!currentAccount && (
             <button
