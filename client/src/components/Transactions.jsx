f2124bc06ada69f4aec62fa8047d4093d5b98880
spiderocta <cryptofer@tuta.io>
2022-07-08 07:01:22 +0200

    mods

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -4,22 +4,21 @@ import { TransactionContext } from "../context/TransactionContext";
 
 import useFetch from "../hooks/useFetch";
 import dummyData from "../utils/dummyData";
-
 import { shortenAddress } from "../utils/shortenAddress";
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
   const gifUrl = useFetch({ keyword });
 
   return (
-    <div className="blue-glassmorphism m-4 flex flex-1
+    <div className="blue-glassmorphism  m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
       sm:min-w-[270px]
       sm:max-w-[300px]
       min-w-full
-      flex-col p-3 rounded-md hover:shadow-2xl blue-glassmorphism"
+      flex-col p-3 rounded-md hover:shadow-2xl"
     >
-      <div className="flex flex-col items-center w-full mt-3 blue-glassmorphism">
+      <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
           <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
