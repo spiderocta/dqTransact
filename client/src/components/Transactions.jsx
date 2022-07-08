38f08d7c8f9b89d8303749c4dbeb34953e8acf0a
spiderocta <cryptofer@tuta.io>
2022-07-08 06:19:14 +0200

    footer and css modifications

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -11,15 +11,15 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
   const gifUrl = useFetch({ keyword });
 
   return (
-    <div className="bg-[#181918] m-4 flex flex-1
+    <div className="blue-glassmorphism m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
       sm:min-w-[270px]
       sm:max-w-[300px]
       min-w-full
-      flex-col p-3 rounded-md hover:shadow-2xl"
+      flex-col p-3 rounded-md hover:shadow-2xl blue-glassmorphism"
     >
-      <div className="flex flex-col items-center w-full mt-3">
+      <div className="flex flex-col items-center w-full mt-3 blue-glassmorphism">
         <div className="display-flex justify-start w-full mb-6 p-2">
           <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
